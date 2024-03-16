b9b6229feb12299496963b98abddbafacc94d65f
Eric Berry <eric@berry.sh>
2024-03-15 22:15:16 -0600

    Remove cruft

--- app/content/helpers/page_helper.rb
+++ app/content/helpers/page_helper.rb
@@ -26,8 +26,4 @@ module PageHelper
   def render_layout(layout, **, &)
     render(html: capture(&), layout: "layouts/#{layout}", **)
   end
-
-  def render_markdown(&block)
-    render MarkdownComponent.new(markdown: block.call)
-  end
 end
